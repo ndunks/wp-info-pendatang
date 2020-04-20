3ebfcaf281585aa6f724c29f09555af6c7d9e8e3
Mochamad Arifin <klampok.child@gmail.com>
2020-04-20 19:45:59 +0700

    Auto dusun

---
+++ ajax_admin/fix_dusun.php
@@ -0,0 +1,19 @@
+<?php
+
+$result = $wpdb->get_results("SELECT id,rw from " . InfoPendatang::$table);
+$rw_maps = [];
+foreach (InfoPendatang::$config['dusun'] as $dusun) {
+    foreach ($dusun['rw'] as $rw) {
+        $rw_maps[$rw] = $dusun['nama'];
+    }
+}
+$updates = [];
+foreach ($result as $row) {
+    if (isset($rw_maps[$row->rw])) {
+        $updates[] = "UPDATE " . InfoPendatang::$table .
+                    " set dusun = '" . addslashes($rw_maps[$row->rw]) .
+                        "' WHERE id = {$row->id}";
+    }
+}
+require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+return dbDelta(implode(";\n", $updates));
